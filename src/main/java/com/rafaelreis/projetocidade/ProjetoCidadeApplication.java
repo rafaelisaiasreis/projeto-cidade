8205e3b930c626d494791e3a82bb233c284db4b3
rafaelisaiasreis <rafaelisaiasreis@gmail.com>
2020-08-18 19:31:03 -0300

    First Commit

---
+++ src/main/java/com/rafaelreis/projetocidade/ProjetoCidadeApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelreis.projetocidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCidadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCidadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafaelreis/projetocidade/ProjetoCidadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelreis.projetocidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCidadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
