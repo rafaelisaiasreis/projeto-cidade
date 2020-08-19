7ab00ab82d93580b29a7a513fb8add9dd76391f4
rafaelisaiasreis <rafaelisaiasreis@gmail.com>
2020-08-18 21:50:15 -0300

    Cria entidade e repository Cidade

---
+++ src/main/java/com/rafaelreis/projetocidade/controllers/CidadeController.java
@@ -0,0 +1,2 @@
+package com.rafaelreis.projetocidade.controllers;public class CidadeController {
+}
---
+++ src/main/java/com/rafaelreis/projetocidade/model/repositories/CidadeRepository.java
@@ -0,0 +1,4 @@
+package com.rafaelreis.projetocidade.model.repositories;
+
+public interface CidadeRepository {
+}
